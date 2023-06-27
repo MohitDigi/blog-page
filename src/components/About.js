e0300f779beb6ecd988076f73d630f4bf19c57f9
Mohit Kalshan <mohit.kalshan@digimantra.com>
2023-06-27 11:27:29 +0530

    css updated and media query changed

--- src/components/About.js
+++ src/components/About.js
@@ -2,80 +2,77 @@ import React from "react";
 import bgimage from "../images/background.png";
 import logo from "../images/logo.svg";
 import sign from "../images/sign.png";
-import india from "../images/india.png"
-import dubai from "../images/dubai.png"
-import usa from "../images/us.png"
+import india from "../images/india.png";
+import dubai from "../images/dubai.png";
+import usa from "../images/us.png";
 import "../styles/about.css";
 
 const About = () => {
   return (
     <>
-      <header></header>
-      <main className="parent-container">
-        {/* Side image */}
-        <div className="main-image-section">
-          <img src={bgimage} alt="bgimage" align="left" />
-        </div>
-        {/* Right Section */}
-        <div className="info-section">
-          <header className="info-header">
-            <div className="logo">
-              <img src={logo} alt="logo" className="logo" />
+      <main id="first">
+        <div class="container">
+          <section class="image">
+            <img src={bgimage} />
+          </section>
+          <section class="message">
+            <img src={logo} id="logo" />
+            <h1>A MESSAGE FROM</h1>
+            <h2>SACHIN KHOSLA ,</h2>
+            <h3>CEO & FOUNDER</h3>
+            <main class="body-content">
+              <p>
+                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+                diam nonumy eirmod tempor invidunt ut labore et dolore magna
+                aliquyam erat, sed diam
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+                diam nonumy eirmod tempor invidunt ut labore et dolore magna
+                aliquyam erat, sed diam voluptua. At vero eos et accusam et
+                justo duo dolores et ea rebum.
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+                diam nonumy eirmod tempor invidunt ut labore et dolore magna
+                aliquyam erat, sed diam voluptua. At vero eos et accusam et
+                justo duo dolores et ea rebum. Lorem ipsum dolor sit amet,
+                consetetur sadipscing elitr, sed diam nonumy eirmod tempor
+                invidunt ut labore et dolore magna aliquyam erat, sed diam
+                voluptua. At vero eos et accusam et justo duo dolores et ea
+                rebum.
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+                diam nonumy eirmod tempor invidunt ut labore et dolore magna
+                aliquyam erat, sed diam
+              </p>
+            </main>
+            <div class="sign">
+              <img src={sign} />
+              <h4>SACHIN KHOSLA ,</h4>
+              <h4>CEO & FOUNDER</h4>
             </div>
-            <div className="about-heading">
-              <h1>A MESSAGE FROM</h1>
-              <h2>SACHIN KHOSLA ,</h2>
-              <h3>CEO & FOUNDER</h3>
-            </div>
-          </header>
-          <main className="info-main-content">
-            <p>
-              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-              nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-              erat, sed diam
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-              nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-              erat, sed diam voluptua. At vero eos et accusam et justo duo
-              dolores et ea rebum.
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-              nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-              erat, sed diam voluptua. At vero eos et accusam et justo duo
-              dolores et ea rebum. Lorem ipsum dolor sit amet, consetetur
-              sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore
-              et dolore magna aliquyam erat, sed diam voluptua. At vero eos et
-              accusam et justo duo dolores et ea rebum.
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-              nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-              erat, sed diam
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-              nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-              erat, sed diam
-            </p>
-            <div className="signature">
-              <img src={sign} alt="sign" />
-              <h2>SACHIN KHOSLA,</h2>
-              <h2>CEO & FOUNDER</h2>
-            </div>
-          </main>
-          {/* Right section footer start */}
-          <footer className="info-footer">
-            <div className="country">
-              <h2>India
-                <img src={india} alt=''/>
-              </h2>
-              <h2>Dubai<img src={dubai} alt=''/></h2>
-              <h2>US<img src={usa} alt=''/></h2>
-            </div>
-            <h2>www.digimantralabs.com</h2>
-          </footer>
+            <hr />
+            <footer class="footer">
+              <ul>
+                <li>
+                  INDIA <img src={india} />
+                </li>
+                <li>
+                  DUBAI <img src={dubai} />
+                </li>
+                <li>
+                  US <img src={usa} />
+                </li>
+              </ul>
+              <p>
+                <a href="https://digimantralabs.com/" target="_blank">
+                  www.digimantralabs.com
+                </a>
+              </p>
+            </footer>
+          </section>
         </div>
       </main>
     </>
